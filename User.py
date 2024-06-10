652b830c1b3e012872c4b32b94a9366b4e476efa
ravenjoswell <rjc.panlaqui@gmail.com>
2024-06-10 10:48:22 -1000

    completed

--- User.py
+++ User.py
@@ -1,4 +1,4 @@
-class User:
+class User():
         def __init__(self, name, email = None, drivers_licence = None, number = None):
             self.name = name
             self.email = email
@@ -6,19 +6,9 @@ class User:
             self.number = number 
         
 
+        def __str__(self):
+             return f"{self.name} | {self.email} | {self.drivers_license} | {self.number} "
 
 
-def test_user_creation():
-    user1 = User("Alice", "alice@example.com")
-    assert user1.name == "Alice"
-    assert user1.email == "alice@example.com"
-    assert user1.drivers_license is None
-    assert user1.number is None
-
-    user2 = User("Bob", "bob@example.com", "XYZ123")
-    assert user2.name == "Bob"
-    assert user2.email == "bob@example.com"
-    assert user2.drivers_license == "XYZ123"
-    assert user2.number is None
 
  
